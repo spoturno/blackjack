6ca5e5aeac55009b8ad1db5910619336336b8f92
tomy-dev <spoturno16@gmail.com>
2021-05-18 02:40:50 -0300

    fixed stand button and added some comments

---
+++ src/js/user.js
@@ -0,0 +1,14 @@
+//learning localstorage
+localStorage.setItem("name", "tomas");
+localStorage.setItem("age", "40");
+console.log(localStorage.getItem("age"));
+
+class User {
+    constructor(){
+
+    }
+
+    
+}
+
+export default User;
